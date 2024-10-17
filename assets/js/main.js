ab669c5b54d458a74cbb936ad31bd5666babf0be
Bruno Costa <b.costamartins@gmail.com>
2024-10-16 21:29:34 -0300

    Make display and buttons work together

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,25 @@
+//Uma chave deve ser referencia com this dentro do próprio objeto
 function criarCalculadora() {
   return {
-    display: document.querySelector(""),
+    display: document.querySelector(".display"),
+
     iniciar() {
-      alert("Olá Mundo");
+      this.cliqueBotoes();
     },
+
     cliqueBotoes() {
-      document.addEventListener("click", function (e) {
+      document.addEventListener("click", (e) => {
+        //com arrow function não preciso me preocupar com o .bin(), this
+        //definido pelo escopo lexico é bem mais tranquilo
         const el = e.target;
+        if (el.classList.contains("btn-num")) {
+          this.btnParaDisplay(el.innerText);
+        }
       });
+    },
+
+    btnParaDisplay(valor) {
+      this.display.value += valor;
     },
   };
 }
